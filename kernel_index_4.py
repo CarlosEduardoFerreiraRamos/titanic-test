4ee3803c36d15780385f565157709c3d32cc068f
Carlos Ramos <carlosefrfloresta@gmail.com>
2018-09-19 08:29:14 -0300

    Added, missing values in categorical features, early implementatio

--- kernel_index_4.py
+++ kernel_index_4.py
@@ -5,12 +5,15 @@ import statsmodels.formula.api as sm;
 from sklearn.preprocessing import StandardScaler;
 from sklearn.linear_model import LogisticRegression;
 from sklearn.preprocessing import StandardScaler;
+from sklearn.preprocessing import Imputer;
 
 print("readFiles")
 """ READ DATA SET FILES """
 df_train = pd.read_csv('data_sets/house_prices_train.csv');
 df_test = pd.read_csv('data_sets/house_prices_test.csv');
 
+
+# print(df_train.isnull().any())
 
 """ CREATE DUMMY VARIABLES """
 categotical_columns = ['MSSubClass','MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities',
@@ -23,22 +26,24 @@ categotical_columns = ['MSSubClass','MSZoning', 'Street', 'Alley', 'LotShape', '
  'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType',
  'SaleCondition'];
 
-for column in  categotical_columns:
-	dummies = pd.get_dummies(df_train[column], prefix=column);
-	df_train = pd.concat([df_train, dummies], axis = 1);
+print(df_train[categotical_columns].isnull().any())
 
-	test_dumies = pd.get_dummies(df_test[column], prefix=column);
-	df_test = pd.concat([df_test, dummies], axis = 1);
+# for column in  categotical_columns:
+# 	dummies = pd.get_dummies(df_train[column], prefix=column);
+# 	df_train = pd.concat([df_train, dummies], axis = 1);
 
-df_train = df_train.drop(columns = categotical_columns);
-df_test = df_test.drop(columns = categotical_columns);
-# print(df_train.columns.values)
-# print(df_test.columns.values)
-""" FUTURE SCALING THE DATA"""
-fs = StandardScaler();
-fs.fit(df_train.loc[:, df_train.columns != "SalePrice"]);
-df_train.loc[:, df_train.columns != "SalePrice"] = fs.transform(df_train.loc[:, df_train.columns != "SalePrice"]);
-df_test = fs.transform(df_test);
+# 	test_dumies = pd.get_dummies(df_test[column], prefix=column);
+# 	df_test = pd.concat([df_test, dummies], axis = 1);
+
+# df_train = df_train.drop(columns = categotical_columns);
+# df_test = df_test.drop(columns = categotical_columns);
+# # print(df_train.columns.values)
+# # print(df_test.columns.values)
+# """ FUTURE SCALING THE DATA"""
+# fs = StandardScaler();
+# fs.fit(df_train.loc[:, df_train.columns != "SalePrice"]);
+# df_train.loc[:, df_train.columns != "SalePrice"] = fs.transform(df_train.loc[:, df_train.columns != "SalePrice"]);
+# df_test = fs.transform(df_test);
 
  
 
