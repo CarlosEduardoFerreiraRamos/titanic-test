7abbdcafa972611522b9767cdd77bf67057034fd
Carlos Ramos <carlosefrfloresta@gmail.com>
2018-11-22 13:05:38 -0200

    run method implemented

--- deep_leaning/fc_ann/neural_network.py
+++ deep_leaning/fc_ann/neural_network.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import expit as activation_function
 class NeuralNetwork(object):
 
     def __init__(self,
@@ -19,3 +20,24 @@ class NeuralNetwork(object):
         rad = 1 / np.sqrt(self.no_of_hidden_nodes)
         X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
         self.weights_hidden_out = X.rvs((self.no_of_out_nodes, self.no_of_hidden_nodes))
+
+    def train(self):
+        pass
+    
+    def run(self, input_vector):
+        """
+        running the network with an input vector input_vector. 
+        input_vector can be tuple, list or ndarray
+        """
+        
+        # turning the input vector into a column vector
+        input_vector = np.array(input_vector, ndmin=2).T
+        output_vector = np.dot(self.weights_in_hidden, input_vector)
+        output_vector = activation_function(output_vector)
+        
+        output_vector = np.dot(self.weights_hidden_out, output_vector)
+        output_vector = activation_function(output_vector)
+    
+        return output_vector
+
+    
