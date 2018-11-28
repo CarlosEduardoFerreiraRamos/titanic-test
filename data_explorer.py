a1e28994a764037cb171a6dd66de9373b73a41bf
Carlos Ramos <carlosefrfloresta@gmail.com>
2018-11-28 12:53:20 -0200

    Added text to index inital

--- data_explorer.py
+++ data_explorer.py
@@ -1,9 +1,48 @@
 """
-Variable Identification
-Univariate Analysis
-Bi-variate Analysis
-Missing values treatment
-Outlier treatment
-Variable transformation
-Variable creation
+Data Exploration and Preparation
+
+1 - Variable Identification
+2 - Univariate Analysis
+3 - Bi-variate Analysis
+4 - Missing values treatment
+5 - Outlier treatment
+6 - Variable transformation
+7 - Variable creation
+"""
+
+"""
+1.Variable Identification
+
+segregate varibles in: 
+    *Type of varible: predictor or target variable;
+    *Data type: Character or numeric;
+    *Varible category: categorical or continuos.
+"""
+
+"""
+2.Univariate Analysis
+At this stage, we explore variables one by one. Method to perform uni-variate analysis will
+depend on whether the variable type is categorical or continuous.
+    Continuous Variables:- In case of continuous variables, we need to understand the central
+    tendency and spread of the variable.
+    Central tendency - Measure of Dispersion - Visualization Method
+    Mean               Range                   Histogram
+    Median             Quartile                Box plot
+    Mode               IQR
+    Min                Standerd Variation
+                       Skewness and Kurtosis
+
+    Categorical Variables:- For categorical variables, we’ll use frequency table to understand
+    distribution of each category. We can also read as percentage of values under each category.
+"""
+
+"""
+3.Bi-variate Analysis
+Bi-variate Analysis finds out the relationship between two variables. Here, we look for association
+and disassociation between variables at a pre-defined significance level. We can perform bi-variate
+analysis for any combination of categorical and continuous variables. The combination can be:
+Categorical & Categorical, Categorical & Continuous and Continuous & Continuous. Different methods
+are used to tackle these combinations during analysis process.
+
+### stop here
 """
