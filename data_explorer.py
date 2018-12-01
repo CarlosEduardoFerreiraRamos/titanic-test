3b0465a15ef2c979317531e437148dc9c30b41c1
Carlos Ramos <carlosefrfloresta@gmail.com>
2018-12-01 16:26:33 -0200

    Implementation, missing data description

--- data_explorer.py
+++ data_explorer.py
@@ -99,3 +99,37 @@ are used to tackle these combinations during analysis process.
           
         - ANOVA:- It assesses whether the average of more than two groups is statistically different.
 """
+
+"""
+3. Missing value treatment
+
+Why missing values treatment is required?
+Missing data in the training data set can reduce the power / fit of a model or can lead to a biased model because
+we have not analysed the behavior and relationship with other variables correctly. It can lead to wrong prediction
+or classification.
+
+Why my data has missing values?
+We looked at the importance of treatment of missing values in a dataset. Now, let’s identify the reasons for
+occurrence of these missing values. They may occur at two stages:
+
+    *Data Extraction: It is possible that there are problems with extraction process. In such cases, we should 
+     double-check for correct data with data guardians. Some hashing procedures can also be used to make sure data
+     extraction is correct. Errors at data extraction stage are typically easy to find and can be corrected easily
+     as well.
+    *Data collection: These errors occur at time of data collection and are harder to correct. They can be categorized
+     in four types:
+        - Missing completely at random: This is a case when the probability of missing variable is same for all
+          observations. For example: respondents of data collection process decide that they will declare their earning
+          after tossing a fair coin. If an head occurs, respondent declares his / her earnings & vice versa. Here each
+          observation has equal chance of missing value.
+        - Missing at random: This is a case when variable is missing at random and missing ratio varies for different
+          values / level of other input variables. For example: We are collecting data for age and female has higher missing
+          value compare to male.
+        - Missing that depends on unobserved predictors: This is a case when the missing values are not random and are related
+          to the unobserved input variable. For example: In a medical study, if a particular diagnostic causes discomfort,
+          then there is higher chance of drop out from the study. This missing value is not at random unless we have included
+          “discomfort” as an input variable for all patients.
+        - Missing that depends on the missing value itself: This is a case when the probability of missing value is directly
+          correlated with missing value itself. For example: People with higher or lower income are likely to provide non-response
+          to their earning.
+"""
